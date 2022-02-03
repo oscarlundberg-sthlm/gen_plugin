ece82a4b72ced41b5cd7f1c9ee95e7bc4cf62c96
oscarlundberg-sthlm <oscarlundberg@hotmail.com>
2022-02-03 13:53:28 +0100

    init

---
+++ gen_plugin.php
@@ -0,0 +1,10 @@
+<?php if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/*
+ * Plugin Name: Generation Blog feed
+ * Description: Shortcode for displaying blog feed
+ * Version: 0.1.0
+ * Author: Oscar Lundberg
+ * Text Domain: generation-blog-feed
+ * Domain Path: /languages
+ */
